5f075157a148abdcd0a7848f790b62de34af1d5b
Zellrei <anthonylaurentweb@gmail.com>
2022-12-29 22:06:39 +0100

    add start btn feature and X board click event)

--- script.js
+++ script.js
@@ -4,17 +4,26 @@ const boardModule = (() => {
                       'x', 'o', 'x'];
   const gameboard = document.getElementById('gameboard');
   const gameboardCells = document.querySelectorAll('.cell');
+  const startBtn = document.getElementById('start-btn');
 
   let clickedCell = "";
   
+  startBtn.addEventListener('click', (event) => {
+    clear();
+    render();
+  });
+
   for (let i = 0; i<9; i++) {
     gameboardCells[i].addEventListener('click', (event) => {
         clickedCell = i;
         console.log('clicked ', i);
         console.log(clickedCell);
-
+        boardModule.playX(clickedCell);
+        boardModule.render();
     });
   }
+  
+
   
   const playX = (a) => gameboardArr[a] = 'x'; 
   const playO = (a) => gameboardArr[a] = 'o'; ;
